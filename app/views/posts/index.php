6d7333f7f1dabec47f7de1cbd135fd2abe37cc6a
caerah99 <syaerahzolkipli@gmail.com>
2026-05-05 16:32:08 +0800

    add post controller

---
+++ app/views/posts/index.php
@@ -0,0 +1,12 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+    <div class="row">
+        <div class="col-md-6">
+            <h1>TULIS PATTTT</h1>
+        </div>
+        <div class="col-md-6">
+            <a href="<?php echo URLROOT; ?>/posts/add" class="btn btn-success pull-right">
+                <i class="fa fa-plus"></i> Add Post
+            </a>
+        </div>
+    </div>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
