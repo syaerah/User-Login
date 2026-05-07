54392f7574a9c063d25d3ee1c123529d48a0a44a
caerah99 <syaerahzolkipli@gmail.com>
2026-05-07 16:57:33 +0800

    insert post

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -21,6 +21,45 @@
         }
 
         public function add(){
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                //sanitize POST array
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                $data = [
+                    'title' => trim($_POST['title']),
+                    'body' => trim($_POST['body']),
+                    'user_id' => $_SESSION['user_id'],
+                    'title_err' => '',
+                    'body_err' => '' 
+                ];
+
+                //validate title
+                if(empty($data['title'])){
+                    $data['title_err'] = 'Please enter title';
+                }
+
+                //validate body
+                if(empty($data['body'])){
+                    $data['body_err'] = 'Please enter body text';
+                }
+
+                //pastikan xde error
+                if(empty($data['title_err']) && empty($data['body_err'])){
+                    //validated
+                    if($this->postModel->addPost($data)){
+                        flash('post_message', 'Post Added');
+                        redirect('posts');
+                    } else {
+                        die('Something went wrong.');
+                    }
+                } else {
+                    //load view ngn errors
+                    $this->view('posts/add', $data);
+                }
+            }
+
+
+
         $data = [
             'title' => '',
             'body' => ''
